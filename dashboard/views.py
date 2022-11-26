172475fa201a72f0085e5b30aeee146f027dcbfc
aviiciii <laavesh1@gmail.com>
2022-11-26 13:55:03 +0530

    db changes

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,18 +1,57 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from DevPort.settings import LOGIN_REDIRECT_URL
 #import messages
 from django.contrib import messages
 
 # Create your views here.
+@login_required(login_url=LOGIN_REDIRECT_URL)
+def logoutpg(request):
+    logout(request)
+    messages.success(request, "Logged out successfully!")
+    return redirect("")
 
-@login_required(login_url="/login/")
+
+@login_required(login_url=LOGIN_REDIRECT_URL)
 def dashboard(request):
+    if request.method == "POST":
+        # get the form data
+        name= request.POST.get('name')
+        role = request.POST.get('role')
+        about = request.POST.get('about')
+        college = request.POST.get('college')
+        course = request.POST.get('course')
+        grade = request.POST.get('grade')
+        graduationyear = request.POST.get('graduationyear')
+        contactemail = request.POST.get('contactemail')
+        instagram= request.POST.get('instagram')
+        twitter = request.POST.get('twitter')
+        github = request.POST.get('github')
+        linkedin = request.POST.get('linkedin')
+
+        # check if user has profile
+        if request.user.profile:
+            # update
+            profile = request.user.profile
+            profile.name = name
+            profile.role = role
+            profile.about = about
+            profile.college = college
+            profile.course = course
+            profile.grade = grade
+            profile.graduationyear = graduationyear
+            profile.contactemail = contactemail
+            profile.instagram = instagram
+            profile.twitter = twitter
+            profile.github = github
+            profile.linkedin = linkedin
+            profile.save()
+            messages.success(request, "Profile updated successfully!")
+            return redirect("dashboard")
+        
     return render(request, 'dashboard/dashboard.html')
 
 
-@login_required
-def logoutpg(request):
-    logout(request)
-    messages.info(request, "Logged out successfully!")
-    return redirect("")
+
+
--- dev_db/models.py
+++ dev_db/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    name= models.CharField(max_length=100)
-    role= models.CharField(max_length=50, default='user')
+    user= models.OneToOneField(User, on_delete=models.CASCADE)
+    name= models.CharField(max_length=100, blank=True)
+    role= models.CharField(max_length=50, blank=True)
     about= models.TextField(max_length=500, blank=True)
     college= models.CharField(max_length=100, blank=True)
     course= models.CharField(max_length=100, blank=True)
